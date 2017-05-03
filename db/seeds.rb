90caeb40d9ac23c36638cb7a00ed8235e3dc2c6f
victorclee <victorchristopherlee@gmail.com>
2017-05-02 21:20:25 -0500

    created product models with attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(
+                      name: "Bertoia Diamond Chair",
+                      price: 1210,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/421L/421L-C_K_(K2026)_K20268_~_FZ.jpg",
+                      description: "Designed by Harry Bertoia in 1952"
+                      )
+product = Product.new(
+                      name: "Barcelona Chair",
+                      price: 5760,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/250L/250L-C_~_(VO)_VOBLK_FZ.jpg",
+                      description: "Designed by Ludwig Mies van der Rohe in 1929"
+                      )
+product = Product.new(
+                      name: "Womb Chair",
+                      price: 3917,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/70L/70L-C_(K1806)_K18062_K18062_~_~_FZ.jpg",
+                      description: "Designed by Eero Saarinen in 1948"
+                      )
+product = Product.new(
+                      name: "Florence Knoll Lounge Chair",
+                      price: 5095,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/1205CS1/1205CS1-(PC)_~_(K1206)_K120613_FZ.jpg",
+                      description: "Designed by Florence Knoll in 1954"
+                      )
+product = Product.new(
+                      name: "Wassily Chair",
+                      price: 2572,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/50L/50L-(PC)_BLBLCK_FZ.jpg",
+                      description: "Designed by Marcel Breuer in 1925"
+                      )
+product = Product.new(
+                      name: "1966 Lounge Chair",
+                      price: 1427,
+                      img: "https://www.knoll.com/static_resources/images/products/catalog/eco/parts/196625/196625-(WWW)_W_W_W_FZ.jpg",
+                      description: "Designed by Richard Schultz in 1966"
+                      )
+
+
+
+
+
+
+
