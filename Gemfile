f33af527481f9603272e18e26c7aaf76cb2ac084
victorclee <victorchristopherlee@gmail.com>
2017-05-09 21:30:24 -0500

    added navbar, CSS font stack, Ruby syntax

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'jquery-rails', '~> 4.3.1'
 gem 'bootstrap-sass', '~> 3.3.7'
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price < 1000
+      return "Discount Item!"
+    else
+      return "Everyday Value!!"
+    end
+  end
+
+  def tax
+    taxation = price * 0.09
+    "The tax for this item is: $ #{taxation}"
+  end
 end
